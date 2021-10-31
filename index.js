d66982187406fe7fbfadd79d2194f3ac6cec6b9f
MD ABDUR RAHMAN <mmm.rahman77@gmail.com>
2021-10-31 12:31:40 +0600

    first commit

--- index.js
+++ index.js
@@ -1,102 +1,79 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-require('dotenv').config()
-const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
-
-const app = express();
-const port = process.env.PORT || 5000;
-// const port = 5000;
-
-// middleware
-app.use(cors());
-app.use(express.json());
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnelf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run() {
-    try {
-        await client.connect();
-        console.log('database connect')
-        const database = client.db('online_shop');
-        const productCollection = database.collection('products');
-        const orderCollection = database.collection('orders');
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const port = 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-        //GET Products API
-        app.get("/products", (req, res) => {
-            productCollection.find({}).toArray((err, results) => {
-                res.send(results);
-            });
-        });
-
-        // Use POST to get data by keys
-        app.post('/products/byKeys', async (req, res) => {
-            const keys = req.body;
-            const query = { key: { $in: keys } }
-            const products = await productCollection.find(query).toArray();
-            res.json(products);
-        });
-
-        // POST API
-        app.post('/products', async (req, res) => {
-            const products = req.body;
-            console.log('hit the post api', products);
-
-            const result = await productCollection.insertOne(products);
-            console.log(result);
-            res.json(result)
-        });
-
-        // Add Orders API
-        app.post('/orders', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.json(result);
-        });
-
-        // GET ORDERS API
-        app.get('/orders', async (req, res) => {
-            const cursor = orderCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
-        });
-
-        //delete product from the database
-        app.delete("/orders/:id", async (req, res) => {
-            console.log(req.params.id);
-
-            orderCollection
-                .deleteOne({ _id: ObjectId(req.params.id) })
-                .then((result) => {
-                    res.send(result);
-                });
-        });
-
-        // get all order by email query
-        app.get("/orders/:email", (req, res) => {
-            console.log(req.params);
-            orderCollection
-                .find({ email: req.params.email })
-                .toArray((err, results) => {
-                    res.send(results);
-                });
-        });
-
-    }
-    finally {
-        // await client.close();
-    }
-}
-
-run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-    res.send('Travel server is running');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log('Server running at port', port);
-})
+client.connect((err) => {
+    const productsCollection = client.db("foodMaster").collection("products");
+    const ordersCollection = client.db("foodMaster").collection("orders");
+
+    //  make route and get data
+    app.get("/products", (req, res) => {
+        productsCollection.find({}).toArray((err, results) => {
+            res.send(results);
+        });
+    });
+
+    // get single prodcut
+    app.get("/products/:id", (req, res) => {
+        console.log(req.params.id);
+        productsCollection
+            .find({ _id: ObjectId(req.params.id) })
+            .toArray((err, results) => {
+                res.send(results[0]);
+            });
+    });
+    //add product
+    app.post("/products", (req, res) => {
+        console.log(req.body);
+        productsCollection.insertOne(req.body).then((documents) => {
+            res.send(documents.insertedId);
+        });
+    });
+
+    //  order get data
+    app.get("/orders", (req, res) => {
+        productsCollection.find({}).toArray((err, results) => {
+            res.send(results);
+        });
+    });
+
+
+    //add order in database
+    app.post("/orders", (req, res) => {
+        ordersCollection.insertOne(req.body).then((result) => {
+            res.send(result);
+        });
+    });
+
+    // get all order by email query
+    app.get("/orders/:email", (req, res) => {
+        console.log(req.params);
+        ordersCollection
+            .find({ email: req.params.email })
+            .toArray((err, results) => {
+                res.send(results);
+            });
+    });
+});
+
+app.listen(process.env.PORT || port);
